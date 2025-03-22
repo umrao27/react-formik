7562ab37553bb39dbff43cedc0abbb1059ab72d6
Rahul Umrao <rahul@Rahuls-MacBook-Pro.local>
2025-03-22 17:11:32 +0530

    document added

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -2,6 +2,10 @@ import { Formik, Form } from "formik";
 import { basicSchema } from "../schemas";
 import CustomInput from "./customFields/CustomInput";
 
+/**
+ * The onSubmit function in this JavaScript React code waits for 1 second after the submit button is
+ * clicked before resetting the form.
+ */
 const onSubmit = async (values, actions) => {
   // wait after submit button click and then reset the form
   await new Promise((resolve) => setTimeout(resolve, 1000));
